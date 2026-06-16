2c5da94d9a1ddddbe24447eb4e822aaabc756e1b
Minato95-ayu <ayushkaushik1441@gmail.com>
2026-06-16 14:52:25 +0530

    feat: add highly optimized python solutions for Arrays and DP

---
+++ Arrays_and_Hashing/1_two_sum.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        """
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return []
